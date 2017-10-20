6c9eb9cd354f0f68c0c09692803ccbfa3cb13f63
Nathan Daniel Peterson <natperson@gmail.com>
2017-10-20 15:27:10 -0700

    set up file system and json dbs

--- models/models.js
+++ models/models.js
@@ -1,7 +1,18 @@
-const db = [1,2,3,4]
+const uuid = require('uuid')
+const dbFake = {1: uuid(),2: uuid(),3: uuid(),4: uuid()}
+const fs = require('fs')
+const path = './db.json'
+
+
+// const books = fs.readFileSync(path, 'utf-8')
+// const bookJS = JSON.parse(books)
+
+
 
 function getAll(){
-  return db
+  const books = fs.readFileSync(path, 'utf-8')
+  const bookJS = JSON.parse(books)
+  return bookJS
 }
 
 module.exports = {getAll}
