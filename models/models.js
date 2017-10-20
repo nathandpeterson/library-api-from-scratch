fbfa43e30ef065e382845dd1f55e6a2d4c141f8d
Nathan Daniel Peterson <natperson@gmail.com>
2017-10-20 15:57:13 -0700

    add dummy data

--- models/models.js
+++ models/models.js
@@ -3,10 +3,8 @@ const dbFake = {1: uuid(),2: uuid(),3: uuid(),4: uuid()}
 const fs = require('fs')
 const path = './db.json'
 
-
 // const books = fs.readFileSync(path, 'utf-8')
 // const bookJS = JSON.parse(books)
-
 
 
 function getAll(){
